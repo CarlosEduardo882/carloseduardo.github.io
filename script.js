801f9e8adf25bad63950e9de699a57128b8402b0
CarlosEduardo882 <carlos.edu278@outlook.com>
2025-07-04 04:10:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 45,
+      "density": { "enable": true, "value_area": 900 }
+    },
+    "color": { "value": "#ff2d95" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 0, "color": "#000000" }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.5,
+      "direction": "none",
+      "out_mode": "bounce"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" },
+      "onclick": { "enable": false }
+    },
+    "modes": {
+      "repulse": { "distance": 100 }
+    }
+  },
+  "retina_detect": true
+});
